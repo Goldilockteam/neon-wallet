50c3ac1aaee45cd58e3fdeaafee79edc61620aa7
Juraj Vitko <juraj.vitko@gmail.com>
2018-06-04 12:20:15 +0300

    local neon-js build;

--- webserv/pack.sh
+++ webserv/pack.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 # has to be run from project root
-node_modules/.bin/cross-env NODE_ENV=production node_modules/.bin/webpack --config ./webserv/webpack.config.webserv --display minimal
 
-cp -f ./webserv/deploy/www/index2.html ./webserv/deploy/www/index.html
+MODE=development
+# MODE=production
+
+pushd ../neon-js
+node_modules/.bin/cross-env \
+  NODE_ENV=${MODE} \
+  node_modules/.bin/webpack \
+  --mode ${MODE} \
+  --display minimal
+popd
+
+NEONJS=webserv/node_modules/\@cityofzion/neon-js/lib/
+cp -f ../neon-js/lib/{browser.js,index.js} ${NEONJS}
+ls -l ${NEONJS}
+
+node_modules/.bin/cross-env \
+  NODE_ENV=${MODE} \
+  node_modules/.bin/webpack \
+  --config webserv/webpack.config.webserv \
+  --display minimal
+
+cp -f webserv/deploy/www/index2.html webserv/deploy/www/index.html
